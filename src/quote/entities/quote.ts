6d0f67d6553cf43abfaf75323a54ec0af74d2231
Damien MILLOT <dmillot@pageup.fr>
2020-10-01 08:42:49 +0200

    Add data annotations

--- src/quote/entities/quote.ts
+++ src/quote/entities/quote.ts
@@ -1,8 +1,18 @@
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
 export class Quote {
 
+  @PrimaryGeneratedColumn()
   public id: number;
+
+  @Column()
   public author: string;
+
+  @Column()
   public philosophy: string;
+
+  @Column()
   public quote: string;
 
   public constructor(author: string, philosophy: string, quote: string) {
