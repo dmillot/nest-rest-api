68cfe6aeaf2b347de21594d55af381dd0a0969a1
Damien MILLOT <dmillot@pageup.fr>
2020-10-18 11:11:55 +0200

    Add DTOs validation

--- src/quote/dto/edit-quote.dto.ts
+++ src/quote/dto/edit-quote.dto.ts
@@ -1,7 +1,17 @@
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class EditQuoteDto {
 
+  @IsNotEmpty()
+  @IsString()
   public readonly author: string;
+
+  @IsNotEmpty()
+  @IsString()
   public readonly philosophy: string;
+
+  @IsNotEmpty()
+  @IsString()
   public readonly quote: string;
 
   public constructor(opts?: Partial<EditQuoteDto>) {
